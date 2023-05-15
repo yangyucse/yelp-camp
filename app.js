2e1b3a3a9fd8156564126078cfd302ce4101ac44
Yu Yang <yangy@LAPTOP-LFR4AADF>
2023-05-15 16:28:32 -0400

    change Models to model

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const path = require("path");
+const User = require("./models/user");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
+
 const methodOverride = require("method-override");
 const ExpressError = require("./utils/ExpressError");
 
