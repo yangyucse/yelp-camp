7a59b5df03ee9cab1ef05d36e195da7181d843b0
Yu Yang <yangy@LAPTOP-LFR4AADF>
2023-05-16 12:55:03 -0400

    cannot set header1

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,7 @@ module.exports.login = (req, res) => {
   //console.log("login:", req.session.returnTo);
   const redirectUrl = req.session.returnTo || "/campgrounds";
   delete req.session.returnTo;
-  res.redirect(redirectUrl);
+  return res.redirect(redirectUrl);
 };
 
 module.exports.logout = (req, res, next) => {
